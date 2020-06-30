d4e505918c626abcfc8b189754ccf7f1c969b542
DannisonArias <dannisonarias@gmail.com>
2020-06-30 13:57:02 -0400

    add more tests

--- test/game.test.js
+++ test/game.test.js
@@ -1,4 +1,4 @@
-import gameStart from './gameMock';
+import gameStart from "./gameMock";
 
 let game;
 
@@ -6,12 +6,18 @@ beforeEach(() => {
   game = gameStart();
 });
 
-describe('test game', () => {
-  test('check for game start', () => {
-    expect(typeof game).toBe('object');
+describe("test game", () => {
+  test("check for game start", () => {
+    expect(typeof game).toBe("object");
   });
-  test('check scenes', () => {
+
+  test("check if all four scenes exist", () => {
     // eslint-disable-next-line
     expect(game.scene._pending.length).toBe(4);
+  });
+
+  test("expect scenes not to be less than 1", () => {
+    // eslint-disable-next-line
+    expect(game.scene._pending.length).toBeGreaterThan(0);
   });
 });
